8304daa2d143c35ce421d60a7ecc34eb79e2ea56
buru0003 <buru0003@algonquinlive.com>
2025-03-08 00:04:45 -0500

    Test: Terraform validation

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,12 +1,13 @@
 # Prefix for various resource names
 variable "labelPrefix" {
   type        = string
-  default = "buru0003"
+  default     = "buru0003"
   description = "This will form the beginning of various resource names."
 }
 
 # Resource group region
 variable "region" {
+  type    = string
   default = "canadacentral"
 }
 
